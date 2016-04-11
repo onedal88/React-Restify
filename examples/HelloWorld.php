4e43db53f846976c2cd8d8fb6bacd7fc30386e5a
oNeDaL <onedal@voodoo.technology>
2016-04-11 15:27:58 +0200

    Modified examples

--- examples/HelloWorld.php
+++ examples/HelloWorld.php
@@ -5,11 +5,9 @@ require '../vendor/autoload.php';
 $server = new CapMousse\ReactRestify\Server("HelloWorldServer", "0.0.0.1");
 
 $server->get('/hello/{name}', function ($request, $response, $next) {
-    $response->write("Hello ".$request->name);
-    next();
+    $response->write("Hello {$request->name}");
+    $response->end();
 });
 
 $runner = new CapMousse\ReactRestify\Runner($server);
-$runner->listen("1337");
-
-echo "aqui";
+$runner->listen(1337);
--- examples/Pre.php
+++ examples/Pre.php
@@ -2,11 +2,11 @@
 
 require '../vendor/autoload.php';
 
-$server = new CapMousse\ReactRestify\Server("HelloWorldServer", "0.0.0.1");
+$server = new CapMousse\ReactRestify\Server("HelloWorldServer", "0.0.0.0");
 
 $server->get('/hello/[name]:any', function ($request, $response, $args) {
     $response->write("Hello ".$args['name']);
 });
 
 $runner = new CapMousse\ReactRestify\Runner($server);
-$runner->listen("1337", "37.59.123.121");
+$runner->listen(1337, "0.0.0.1");
